65169ad67c3023dc94c5ef569e9c91f39b48a44e
Mauricio Saavedra <masavi2008@hotmail.com>
2016-06-06 06:16:09 -0500

    Primer script de la base de datos

---
+++ Documentación/db_script/super.sql
@@ -0,0 +1,342 @@
+-- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: supermercado
+-- ------------------------------------------------------
+-- Server version	10.1.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `caja`
+--
+
+DROP TABLE IF EXISTS `caja`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `caja` (
+  `id_caja` int(11) NOT NULL,
+  `tipo_caja` varchar(50) NOT NULL,
+  `id_cajero` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_caja`),
+  KEY `id_cajero` (`id_cajero`),
+  CONSTRAINT `caja_ibfk_1` FOREIGN KEY (`id_cajero`) REFERENCES `cajero` (`id_cajero`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `caja`
+--
+
+LOCK TABLES `caja` WRITE;
+/*!40000 ALTER TABLE `caja` DISABLE KEYS */;
+/*!40000 ALTER TABLE `caja` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cajero`
+--
+
+DROP TABLE IF EXISTS `cajero`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cajero` (
+  `id_cajero` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `genero` char(1) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `id_venta` int(11) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_cajero`),
+  KEY `id_venta` (`id_venta`),
+  CONSTRAINT `cajero_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cajero`
+--
+
+LOCK TABLES `cajero` WRITE;
+/*!40000 ALTER TABLE `cajero` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cajero` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `corporativo`
+--
+
+DROP TABLE IF EXISTS `corporativo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `corporativo` (
+  `id_corporativo` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_corporativo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `corporativo`
+--
+
+LOCK TABLES `corporativo` WRITE;
+/*!40000 ALTER TABLE `corporativo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `corporativo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `delegacion`
+--
+
+DROP TABLE IF EXISTS `delegacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `delegacion` (
+  `id_delegacion` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `id_estado` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_delegacion`),
+  KEY `id_estado` (`id_estado`),
+  CONSTRAINT `delegacion_ibfk_1` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `delegacion`
+--
+
+LOCK TABLES `delegacion` WRITE;
+/*!40000 ALTER TABLE `delegacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `delegacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `descuento`
+--
+
+DROP TABLE IF EXISTS `descuento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `descuento` (
+  `id_descuento` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_limite` date NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_descuento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `descuento`
+--
+
+LOCK TABLES `descuento` WRITE;
+/*!40000 ALTER TABLE `descuento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `descuento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalleventa`
+--
+
+DROP TABLE IF EXISTS `detalleventa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detalleventa` (
+  `id_venta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_descuento` int(11) NOT NULL,
+  PRIMARY KEY (`id_venta`,`id_producto`),
+  KEY `id_producto` (`id_producto`),
+  KEY `id_descuento` (`id_descuento`),
+  CONSTRAINT `detalleventa_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
+  CONSTRAINT `detalleventa_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
+  CONSTRAINT `detalleventa_ibfk_3` FOREIGN KEY (`id_descuento`) REFERENCES `descuento` (`id_descuento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalleventa`
+--
+
+LOCK TABLES `detalleventa` WRITE;
+/*!40000 ALTER TABLE `detalleventa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalleventa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estado`
+--
+
+DROP TABLE IF EXISTS `estado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estado`
+--
+
+LOCK TABLES `estado` WRITE;
+/*!40000 ALTER TABLE `estado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `linea` varchar(50) NOT NULL,
+  `id_sucursal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `id_sucursal` (`id_sucursal`),
+  CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sucursal`
+--
+
+DROP TABLE IF EXISTS `sucursal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sucursal` (
+  `id_sucursal` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(300) NOT NULL,
+  `id_delegacion` int(11) DEFAULT NULL,
+  `id_corporativo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_sucursal`),
+  KEY `id_delegacion` (`id_delegacion`),
+  KEY `id_corporativo` (`id_corporativo`),
+  CONSTRAINT `sucursal_ibfk_1` FOREIGN KEY (`id_delegacion`) REFERENCES `delegacion` (`id_delegacion`),
+  CONSTRAINT `sucursal_ibfk_2` FOREIGN KEY (`id_corporativo`) REFERENCES `corporativo` (`id_corporativo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sucursal`
+--
+
+LOCK TABLES `sucursal` WRITE;
+/*!40000 ALTER TABLE `sucursal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sucursal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipopago`
+--
+
+DROP TABLE IF EXISTS `tipopago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipopago` (
+  `id_pago` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_pago`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipopago`
+--
+
+LOCK TABLES `tipopago` WRITE;
+/*!40000 ALTER TABLE `tipopago` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipopago` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipopago_venta`
+--
+
+DROP TABLE IF EXISTS `tipopago_venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipopago_venta` (
+  `id_pago` int(11) NOT NULL,
+  `id_venta` int(11) NOT NULL,
+  PRIMARY KEY (`id_pago`,`id_venta`),
+  KEY `id_venta` (`id_venta`),
+  CONSTRAINT `tipopago_venta_ibfk_1` FOREIGN KEY (`id_pago`) REFERENCES `tipopago` (`id_pago`),
+  CONSTRAINT `tipopago_venta_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipopago_venta`
+--
+
+LOCK TABLES `tipopago_venta` WRITE;
+/*!40000 ALTER TABLE `tipopago_venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipopago_venta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venta` (
+  `id_venta` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `id_sucursal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `id_sucursal` (`id_sucursal`),
+  CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-06  6:14:21
